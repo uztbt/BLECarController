752601f9376b0713eb8c3f9028c60cf2c4f16ab3
Yuji Tabata <yujitabata@icloud.com>
2020-05-11 19:16:53 +0900

    Initial Commit

---
+++ BLECarControllerTests/BLECarControllerTests.swift
@@ -0,0 +1,33 @@
+//
+//  BLECarControllerTests.swift
+//  BLECarControllerTests
+//
+//  Created by tabatay89 on 2020/05/11.
+//
+
+import XCTest
+@testable import BLECarController
+
+class BLECarControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
